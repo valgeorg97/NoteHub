782939c1155719885df18b4b3231fc3cd77ef51b
valgeorg97 <valentingg1997@gmail.com>
2024-04-29 13:03:10 +0300

    Format note created at date

--- frontend/src/components/NotesCard.tsx
+++ frontend/src/components/NotesCard.tsx
@@ -2,7 +2,6 @@ import { NotesCardProps } from "../types";
 import { RiCloseFill } from "react-icons/ri";
 
 const NotesCard = ({ note, onDelete, onNoteClick }: NotesCardProps) => {
-
     const handleDelete = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.stopPropagation(); 
         onDelete(note.id);
@@ -12,6 +11,8 @@ const NotesCard = ({ note, onDelete, onNoteClick }: NotesCardProps) => {
         onNoteClick(note);
     };
 
+    const formattedDate = note.created_at?.substring(0, 10) ?? '';
+
     return (
         <div className={`relative block w-full h-full p-4 rounded-lg shadow-xl hover:scale-105 
         hover:cursor-pointer bg-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700`} onClick={handleClick}>
@@ -19,7 +20,7 @@ const NotesCard = ({ note, onDelete, onNoteClick }: NotesCardProps) => {
             className="absolute top-2 right-2 text-gray-800 cursor-pointer hover:text-gray-500">
                 <RiCloseFill size={28} />
             </div>
-            <h2 className="mb-2 text-sm font-bold tracking-tight text-gray-600 dark:text-white">{note.created_at}</h2>
+            <h2 className="mb-2 text-sm font-bold tracking-tight text-gray-600 dark:text-white">{formattedDate}</h2>
             <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{note.title}</h1>
             <p className="font-normal text-gray-700 dark:text-gray-400">{note.content}</p>
         </div>
